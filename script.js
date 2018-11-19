dd58b0922a790087c5e54cc47a7ce29c79dd94e3
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-19 14:43:47 +0100

    fix NaN

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* 1. Écris une fonction my_max() qui prend une array et qui retourne le nombre maximum */
 let my_max = [-1, 5, 2];
-console.log(Math.max(my_max));
+console.log(Math.max(...my_max));
 
 /* 2. Écris une fonction vowel_count() qui prend un string et qui retourne le nombre de voyelles */
 let vowel_count = 'combien de voyelles ?'.match(/[aeiouy]/gi);
