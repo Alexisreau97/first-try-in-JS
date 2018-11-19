0a12e21a19dba1a88066e2ed23a4c0b7ca71a781
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-19 10:43:31 +0100

    add vowel_count

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ let my_max = [-1, 5, 2];
 console.log(Math.max(...my_max));
 
 /* 2. Écris une fonction vowel_count() qui prend un string et qui retourne le nombre de voyelles */
-let vowel_count = 'azertyuiop';
+let vowel_count = 'combien de voyelles ?'.match(/[aeiouy]/gi);
 console.log(vowel_count.length);
+
+/* 3. Écris une fonction reverse() qui prend un string et qui renvoie les caractères en position inverse */
